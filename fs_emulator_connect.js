d6705a01a69535919caa3ce3707c63a64f1d10b3
Carlos Parra <carlosparramtz@hotmail.com>
2024-04-26 12:58:03 -0600

    updated dependencies

--- fs_emulator_connect.js
+++
@@ -1,5 +0,0 @@
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
-
-// firebaseApps previously initialized using initializeApp()
-const db = getFirestore();
-connectFirestoreEmulator(db, '127.0.0.1', 8080);
